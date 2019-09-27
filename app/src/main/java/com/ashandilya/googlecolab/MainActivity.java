b08f8b1042eecffffbdafc93f772f4faa06bb0f5
Anshu Kumar Shandilya <ashandilya64@gmail.com>
2019-09-27 21:52:46 +0530

    Update

--- app/src/main/java/com/ashandilya/googlecolab/MainActivity.java
+++ app/src/main/java/com/ashandilya/googlecolab/MainActivity.java
@@ -7,12 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.material.circularreveal.cardview.CircularRevealCardView;
+
 public class MainActivity extends AppCompatActivity {
+
+    CircularRevealCardView cardViewSuggest, cardViewRate, cardViewShare, cardViewKudos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardViewSuggest = findViewById(R.id.suggestion);
+        cardViewRate = findViewById(R.id.rate);
+        cardViewShare = findViewById(R.id.share);
+        cardViewKudos = findViewById(R.id.kudos);
 
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +35,17 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void suggestion(View view) {
+    }
+
+    public void rate(View view) {
+    }
+
+    public void share(View view) {
+    }
+
+    public void kudos(View view) {
     }
 }
